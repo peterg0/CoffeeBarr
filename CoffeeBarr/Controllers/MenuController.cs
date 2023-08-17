d3b82c19841606cd7b0424485c6ff564033a5453
peterg0 <75237739+peterg0@users.noreply.github.com>
2023-08-16 20:50:46 -0500

    added google sheets integration

--- CoffeeBarr/Controllers/MenuController.cs
+++ CoffeeBarr/Controllers/MenuController.cs
@@ -1,12 +1,23 @@
 ﻿using CoffeeBarr.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.IO;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Auth.OAuth2;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace CoffeeBarr.Controllers
 {
     public class MenuController : Controller
     {
         private readonly ILogger<MenuController> _logger;
+        static readonly string[] Scopes = { SheetsService.Scope.SpreadsheetsReadonly };
+        static readonly string ApplicationName = "Menu";
+        static readonly string SpreadsheetID = "1B0AYOxFFn1NLa0NyzFLtn5zDLDEu9icBjdyuIV4df3g";
+        static readonly string sheet = "MenuItems";
+        static SheetsService service;
 
         public MenuController(ILogger<MenuController> logger)
         {
@@ -15,7 +26,34 @@ namespace CoffeeBarr.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            GoogleCredential credential;
+            using (var stream = new FileStream("client_secrets.json", FileMode.Open, FileAccess.Read))
+            {
+                credential = GoogleCredential.FromStream(stream).CreateScoped(Scopes);
+            }
+
+            service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Fetch data from Google Sheets
+            var data =  GetMenuDataFromSheets();
+
+            // Pass the fetched data to the view
+            return View(data);
+        }
+
+        private List<IList<object>> GetMenuDataFromSheets()
+        {
+            var range = $"{sheet}!A1:H";
+            var request = service.Spreadsheets.Values.Get(SpreadsheetID, range);
+            
+            var response = request.Execute();
+            var values = response.Values.ToList();
+
+            return values;
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ CoffeeBarr/Models/MenuItem.cs
@@ -0,0 +1,14 @@
+﻿namespace CoffeeBarr.Models
+{
+    public class MenuItem
+    {
+        public string Category { get; set; }
+        public string ItemName { get; set; }
+        public string Description { get; set; }
+        public string SmallPrice { get; set; }
+        public string MediumPrice { get; set; }
+        public string LargePrice { get; set; }
+        public string Price { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
